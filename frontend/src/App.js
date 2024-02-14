ea1f00f25442d7165e7452c5bedcc7f655e8d711
Priyanshu <Priyanshu.k4444@gmail.com>
2024-02-14 23:15:14 +0530

    pj

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AppNavbar from "./Components/Navbar";
 import Appfooter from "./Components/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
-// import C1 from "./Components/C1";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <AppNavbar />
-        <C1 />
+        {/* <C1 /> */}
+        <Appfooter />
       </div>
     </Router>
   );
