f76b9e69ac7da8dbcbb84df136158bf899b6df10
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2024-02-16 20:31:19 +0530

    czc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AppNavbar from "./Components/Navbar";
 import Appfooter from "./Components/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <AppNavbar />
+        <Home/>
         <Appfooter />
       </div>
     </Router>
--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import First from "./componentHome/First";
 function home() {
   return <div>
-    
+    <First/>
   </div>;
 }
 
