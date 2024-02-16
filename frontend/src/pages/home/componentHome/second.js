1d256929f72f2ce328d0015f2d91c57f59f34d5e
Priyanshu <Priyanshu.k4444@gmail.com>
2024-02-17 02:03:59 +0530

    fifth

---
+++ frontend/src/pages/home/componentHome/second.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./second.css";
+import Lottie from "lottie-react";
+import Marquee from "../../../Components/Marquee";
+
+export default function second() {
+  const logos = [
+    require("./google.png"),
+    require("./google.png"),
+    require("./google.png"),
+    require("./cipher.png"),
+    require("./amazon.png"),
+    require("./google.png"),
+    require("./google.png"),
+    require("./google.png"),
+  ]; // Replace with your logo URLs
+  const speed = "20s"; // Adjust the speed of the marquee animation
+
+  return (
+    <div className="second">
+      <div className="box1">
+        <div className="texthead h1">
+          <p className="h1 text-head">Top Companies Hiring Now !!</p>
+        </div>
+        <div className="icon-flex">
+          <div className="animation-container">
+            <Marquee logos={logos} speed={speed} />
+          </div>
+        </div>
+        <div className="box2">
+          <button className="btn-connect">Connect With Us</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import First from "./componentHome/first";
+import Second from "./componentHome/second";
+import Fifth from "./componentHome/fifth";
+
 function home() {
   return (
     <div>
       <First />
+      <Second />
+      <Fifth />
     </div>
   );
 }
