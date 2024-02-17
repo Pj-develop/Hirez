7f87c1269f7d75956ec965a816d25240fd801fe5
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2024-02-17 19:45:36 +0530

    first

--- frontend/src/pages/home/componentHome/third.js
+++ frontend/src/pages/home/componentHome/third.js
@@ -7,11 +7,11 @@ export default function third() {
       <h2 id="Head">Recruit talent across <br/>domains</h2>
       <p className='p1'>Talent Acquisition Made Easy: Effortless <br/>
  Registration, Simplified Job Posting, and Faster Applications</p>
-    {/* <div className="features">
-        <ul>hello</ul>
-        <ul>hello</ul>
-        <ul>hello</ul>
-    </div> */}
+    <div className="featres">
+        <div><ul className='feat1'><h2 className='feat1H'>Register with us</h2><p className='feat1P'>use your company mail to create your account <br /> and start posting jobs </p></ul></div>
+        <div><ul className='feat2'><h2 className='feat2H'>Post your jobs</h2><p>use your company mail to create your account<br /> and start posting jobs </p></ul></div>
+        <div><ul className='feat3'><h2 className='feat3H'>Hire Best talent</h2><p>Let our AI help find the best talent fast<br /> and start posting jobs</p></ul></div>
+    </div>
     </div>
   )
 }
