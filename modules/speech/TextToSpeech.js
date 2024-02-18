48f098307d3beb985c14ba3cf33ed2229d162acd
Developer <66026094+mokbhai@users.noreply.github.com>
2024-02-18 17:27:22 +0530

    fianl

--- modules/speech/TextToSpeech.js
+++ modules/speech/TextToSpeech.js
@@ -1,12 +1,13 @@
-import gTTS from 'gtts';
-const TextToSpeech	= (Text)=>{
-let speech = Text;
-const gtts = new gTTS(speech, 'en');
+import gTTS from "gtts";
+const TextToSpeech = (Text) => {
+  let speech = Text;
+  const gtts = new gTTS(speech, "en");
 
-gtts.save('Voice.mp3', function (err, result){
-	if(err) { throw new Error(err); }
-	console.log("Text to speech converted!");
-});
-
-}
-TextToSpeech("hello i am hemant singh")
+  gtts.save("Voice.mp3", function (err, result) {
+    if (err) {
+      throw new Error(err);
+    }
+    console.log("Text to speech converted!");
+  });
+};
+export default TextToSpeech;
